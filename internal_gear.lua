a8b0bda54d233ec11331f424d44468ee31bd0d2d
Arash Kadkhodaei <kad.arash@email.com>
2022-06-08 00:33:57 +0200

    Fixed problems

--- internal_gear.lua
+++ internal_gear.lua
@@ -9,7 +9,8 @@
 
 
 ---- Input parameters using Interface
-z_n = ui_numberBox("Number Of Teeth for External Gear", 25);  --number of teeth
+z_n = ui_numberBox("Number Of Teeth for External Gear", 25);  --number of teeth external
+z_1 = ui_numberBox("Number Of Teeth for Internal Gear", 17);  --number of teeth internal
 m = ui_numberBox("Module Of Gear", 4);  -- Module
 alpha_t = ui_scalarBox("Pressure Angle", 20, 1);  -- Pressure angle
 h_a_coef_p = ui_scalarBox("Addendum Coef(mm)", 1, 0);  -- Addendum height
@@ -41,64 +42,59 @@ function mirror(point)
 end
 
 ---- Function for  Internal and External Gear Profiles
-function gearProfile(z, m_n, alpha_t, x_coef, h_a_coef, h_f_coef, b, w,c_t)
+function gearProfile(z, m_n, alpha_t, x_coef, h_a_coef, h_f_coef, b)
     local inv_xy = {}
-    
-    alpha = alpha_t * math.pi / 180;  -- Pressure angle
-    h_a_coef = h_a_coef;  -- Addendum coefficient
-    h_f_coef = h_f_coef;  -- Dedendum coefficient
-    c = 0.1 * m_n;  -- Clearance
-    x_coef = x_coef;  -- Profile shift
-    if x_coef<0 then
-        x_coef=0;
-    end
-  
 
-w=3;
+    local alpha = alpha_t * math.pi / 180;  -- Pressure angle
+    c = 0.1 * m_n;  -- Clearance
+
+    -- x_coef is Profile shift
+    if x_coef < 0 then
+        x_coef = 0;
+    end
+
     -- Formula calculations for the gear profile
 
-    d_p = z * m_n -- Pitch Diamter
-    r_p = d_p / 2 -- Pitch radius
+    local d_p = z * m_n -- Pitch Diamter
+    local r_p = d_p / 2 -- Pitch radius
 
-    d_b = d_p * math.cos(alpha);  -- Base diameter of gear
+    local d_b = d_p * math.cos(alpha);  -- Base diameter of gear
     r_b = d_b / 2 -- Base radius
 
-    h_f = m_n * (h_f_coef) -- Dedendum
-    
-   
-    h_a = m_n * (h_a_coef);  -- Addendum
-    d_a = (d_p + (2 * m_n * (1 + x_coef))); 
-    r_a = d_a / 2; -- Addendum radius
+    -- h_f_coef is dedendum coefficient
+    local h_f = m_n * (h_f_coef) -- Dedendum
 
-    d_f = m_n*z+ 2*x_coef*m_n - 2*(h_f);
-    r_f = d_f / 2; -- Root_radius
+    -- h_a_coef is addendum coefficient
+    local h_a = m_n * (h_a_coef);  -- Addendum
+    local d_a = (d_p + (2 * m_n * (1 + x_coef)));
+    r_a = d_a / 2;  -- Addendum radius
+
+    local d_f = m_n * z + 2 * x_coef * m_n - 2 * (h_f);
+    r_f = d_f / 2;  -- Root_radius
 
     -- Involute function
     inv_a = math.tan(alpha) - alpha;
 
     -- True Involute Diameter:
-    fp = ((m_n * ((math.pi / 4) - (math.tan(alpha))) - c * math.tan(alpha)) * (1 + math.sin(alpha))) / (math.cos(alpha))
-    d_TIF = math.sqrt(math.pow(d_p * math.sin(alpha) - 2 * (h_a - (m_n * x_coef) - fp * (1 - math.sin(alpha))) / (math.sin(alpha)), 2) + d_b * d_b) -- True involute diameter
+    local fp = ((m_n * ((math.pi / 4) - (math.tan(alpha))) - c * math.tan(alpha)) * (1 + math.sin(alpha))) / (math.cos(alpha))
+    local d_TIF = math.sqrt(math.pow(d_p * math.sin(alpha) - 2 * (h_a - (m_n * x_coef) - fp * (1 - math.sin(alpha))) / (math.sin(alpha)), 2) + d_b * d_b) -- True involute diameter
     r_TIF = d_TIF / 2;  --radius
 
     -- Tooth thickness -->> pitch circle
-    S_0 = m_n * ((math.pi / 2) + 2 * x_coef * math.tan(alpha));
+    local S_0 = m_n * ((math.pi / 2) + 2 * x_coef * math.tan(alpha));
 
     -- Tooth thickness on the form circle
-    alpha_f     = math.acos((d_p * math.cos(alpha)) / d_TIF);  -- Involute angle along form circle
-    invFunction = math.tan(alpha_f) - alpha_f;  -- Involute function
-    s_f         = d_TIF * ((S_0 / d_p) + inv_a - invFunction) + (x_coef * math.tan(alpha_f));  -- Thickness of the gear teeth along form circle
-    angel       = (s_f) / (0.5 * d_TIF);  -- Angle swept along form circle for corresponding thickeness
+    local alpha_f     = math.acos((d_p * math.cos(alpha)) / d_TIF);  -- Involute angle along form circle
+    local invFunction = math.tan(alpha_f) - alpha_f;  -- Involute function
+    local s_f         = d_TIF * ((S_0 / d_p) + inv_a - invFunction) + (x_coef * math.tan(alpha_f));  -- Thickness of the gear teeth along form circle
+    local angel       = (s_f) / (0.5 * d_TIF);  -- Angle swept along form circle for corresponding thickeness
 
 
     -- loops for generation points
-
-    tooth_ang = (((math.pi * m_n / 2) + 2 * m_n * x_coef * math.tan(alpha)) / r_p + 2 * math.tan(alpha) - 2 * alpha)
-    steps = 30;
+    local steps = 30;
     for i = 1, z do
-        th = 2 * math.pi
-        AngelS = involute_angle(r_b, r_TIF);  -- Start angle
-        AngelE = involute_angle(r_TIF, r_a);  -- End asngle
+        local th = 2 * math.pi
+        local AngelE = involute_angle(r_TIF, r_a);  -- End asngle
         for j = 1, steps do
             inv_xy[#inv_xy + 1] = p_rotate(th * i / z, involute_t(r_TIF, ((AngelE) * j / steps))) -- firsr involute face
         end
@@ -181,24 +177,20 @@ end
 
 ---- Function Working Pressure angle
 function WorkingAngelF(x) -- Acoording to 1992 [Harry Cheng], derivation of an explicit solution of the inverse involute function
- return (((math.pow(3 * x, (1 / 3)))) - (2 * x / 5) + (math.pow(9 / 175 * 3, (2 / 3))) * (math.pow(x, (5 / 3))) - (math.pow(2 / 175 * 3, (1 / 3))) * (math.pow(x, (7 / 3))) - ((144 / 67375) * (math.pow(x, (9 / 3))) + (3258 / 3128215) * (math.pow(3, (2 / 3))) * (math.pow(x, (11 / 3))) - (49711 / 153278125) * (math.pow(3, (1 / 3))) * (math.pow(x, (13 / 3))) - (1130112 / 9306171875) * (math.pow(x, (15 / 3))) + (5169659643 / 95304506171875) * (math.pow(3, (2 / 3))) * (math.pow(x, (17 / 3)))))
+    return (((math.pow(3 * x, (1 / 3)))) - (2 * x / 5) + (math.pow(9 / 175 * 3, (2 / 3))) * (math.pow(x, (5 / 3))) - (math.pow(2 / 175 * 3, (1 / 3))) * (math.pow(x, (7 / 3))) - ((144 / 67375) * (math.pow(x, (9 / 3))) + (3258 / 3128215) * (math.pow(3, (2 / 3))) * (math.pow(x, (11 / 3))) - (49711 / 153278125) * (math.pow(3, (1 / 3))) * (math.pow(x, (13 / 3))) - (1130112 / 9306171875) * (math.pow(x, (15 / 3))) + (5169659643 / 95304506171875) * (math.pow(3, (2 / 3))) * (math.pow(x, (17 / 3)))))
 end
 
 function center() --Calculation for the centre using the profile shift coefficients
-    alpha_rad = alpha_t * math.pi / 180;
-    z_2 = z_n;  -- Number of teeth Internal Gear
-    z_1 = z_n - 8;  -- Number of teeth External Gear
-
-    alpha_rad = alpha_t * math.pi / 180 -- Preassure angle
-    inv_a = math.tan(alpha_rad) - alpha_rad;  -- Involute function
-    inv_aw = ((2 * math.tan(alpha_rad) * (x_coef_ext-x_coef_int)) / (z_2 - z_1)) + inv_a;  -- Involute function working pressure angle
+    local alpha_rad = alpha_t * math.pi / 180;  -- Preassure angle
+    local inv_a = math.tan(alpha_rad) - alpha_rad;  -- Involute function
+    local inv_aw = ((2 * math.tan(alpha_rad) * (x_coef_ext - x_coef_int)) / (z_n - z_1)) + inv_a;  -- Involute function working pressure angle
 
     -- Working preassure angle
-    alpha_aw = WorkingAngelF(inv_aw);
+    local alpha_aw = WorkingAngelF(inv_aw);
     -- Centre distance coeficiant factor
-    y = ((z_2 - z_1) * (math.cos(alpha_rad) - math.cos(alpha_aw)))/ (2* math.cos(alpha_aw));
+    local y = ((z_n - z_1) * (math.cos(alpha_rad) - math.cos(alpha_aw))) / (2 * math.cos(alpha_aw));
     -- Center distance between Gears
-    a_x = (((z_2 - z_1) / 2) + y) * m;  -- Center distance
+    local a_x = (((z_n - z_1) / 2) + y) * m;  -- Center distance
     meshDistance = a_x;
 end
 
@@ -206,31 +198,43 @@ center();
 
 function gear_formation()
     -- Formation of rotor gear
-    rotor_gear = gearProfile(z_n, m, alpha_t, x_coef_int, h_a_coef_p, h_f_coef_p, b,2);  -- Rotor gear
-    rot_rotor = rotate(0, 0, rotation);  -- Rotation of rotor gear
+    local rotor_gear = gearProfile(z_n, m, alpha_t, x_coef_int, h_a_coef_p, h_f_coef_p, b);  -- Rotor gear
+    local rot_rotor = rotate(0, 0, rotation);  -- Rotation of rotor gear
     emit(translate(0, 0, -0.1) * cylinder(r_a, 2), 1); -- Rotor gear Base formation
     emit(rot_rotor * difference(extrude(circle(r_a - 0.1), 0, v(0, 0, b), v(1, 1, 1), 20), rotor_gear), 1); -- Rotor gear formation
     set_brush_color(1, 0.3, 0.3, 0.4); -- Set color for rotor gear
     -- Parameters for the crescent calculation
-    crescent_outer_radius = r_TIF;  -- Radius of the rotor gear
-    crescent_root_radius = r_f;  -- Root radius of the rotor gear
+    local crescent_outer_radius = r_TIF;  -- Radius of the rotor gear
+    local crescent_root_radius = r_f;  -- Root radius of the rotor gear
     -- Formation of Gear housing
-    gear_housing = difference(cylinder(r_a + 16, b), cylinder(r_a + 1.75, b));  -- Gear housing cylinder
-    inlet = rotate(45, Z) * translate((z_n * m) / 2 + m * 2 + 16, 0, b / 2 + 0.2) * (rotate(270, Y) * cylinder(b / 2 - 1, (z_n * m) / 2 + m * 2 + 16));  -- Inlet hole
-    outlet = rotate(45, Z) * translate(0, (z_n * m) / 2 + m * 2 + 16, b / 2 + 0.2) * (rotate(270, -X) * cylinder(b / 2 - 1, (z_n * m) / 2 + m * 2 + 16));  -- Outlet hole
+    local gear_housing = difference(cylinder(r_a + 16, b), cylinder(r_a + 1.75, b));  -- Gear housing cylinder
+
+    local h = (z_n * m) / 2 + m * 2 + 16;
+    local inlet_trans = rotate(45, Z) * translate(h, 0, b / 2 + 0.2) * rotate(270, Y); -- Inlet translate
+    local outlet_trans = rotate(45, Z) * translate(0, h, b / 2 + 0.2) * rotate(270, -X) * rotate(90, -Z); -- Outlet translate
+    local incyl1 = inlet_trans * cylinder(b / 2 - 1, h);  -- Inlet cylinder 1
+    local incyl2 = inlet_trans * translate(0, b - 2, 0) * cylinder(b / 2 - 1, h);  -- Inlet cylinder 2
+    local incube = inlet_trans * translate(0, b / 2 - 1, 0) * cube(b - 2, b - 1, h); -- Inlet cube
+    local inlet = union(union(incyl1,incyl2),incube); -- Inlet hole
+    local outcyl1 = outlet_trans * cylinder(b / 2 - 1, h);  -- Outlet cylinder 1
+    local outcyl2 = outlet_trans * translate(0, b - 2, 0) * cylinder(b / 2 - 1, h);  -- Outlet cylinder 2
+    local outcube = outlet_trans * translate(0, b / 2 - 1, 0) * cube(b - 2, b - 1, h); -- Outlet cube
+    local outlet = union(union(outcyl1,outcyl2),outcube); -- Outlet hole
     emit(intersection(difference(gear_housing, inlet), difference(gear_housing, outlet)), 0); -- Gear housing formation
+    
+
     emit(translate(0, 0, -0.5) * cylinder(r_a + 16, 0.5), 0); -- Base for Gear housing formation
     -- Formation of idler gear
-    idler_gear = gearProfile(z_n - 8, m, alpha_t, x_coef_ext, h_a_coef_p, h_f_coef_p, b,1);  -- Idler gear
-    rot_idler = rotate(0, 0, rotation * z_n / (z_n - 8));  -- Rotation of idler gear
+    local idler_gear = gearProfile(z_1, m, alpha_t, x_coef_ext, h_a_coef_p, h_f_coef_p, b);  -- Idler gear
+    local rot_idler = rotate(0, 0, rotation * z_n / z_1);  -- Rotation of idler gear
     emit(translate(0, meshDistance, 0) * rot_idler * cylinder(2 + r_b / 2, r_b + 15), 2); -- Shaft formation
     emit(translate(0, meshDistance, 0) * rot_idler * difference(idler_gear, extrude(circle(r_b / 2), 0, v(0, 0, b), v(1, 1, 1), 20)), 2); -- Idler gear formation
     set_brush_color(2, 0, 0, 0); -- Set color for idler gear
     -- Formation of the crescent
-    crescent_circle = translate(0, meshDistance, 0) * ccylinder(r_a + c * 2, b);  -- Cylinder for crescent
-    crescent_cube_right = translate(r_a + c, meshDistance + m * 2, 0) * cube(crescent_root_radius + c, crescent_root_radius + c, b + 0.1);  -- Right cube to remove sharp edges of the crescent
-    crescent_cube_left = translate(-(r_a), meshDistance + m * 2, 0) * cube(crescent_root_radius + c, crescent_root_radius + c, b + 0.1);  -- Left cube to remove sharp edges of the crescent
-    crescent_main = translate(0, 0, b / 2 + 0.1) * difference(ccylinder(crescent_outer_radius - c * 2, b), crescent_circle);  -- Crescent with sharp edges
+    local crescent_circle = translate(0, meshDistance, 0) * ccylinder(r_a + c * 2, b);  -- Cylinder for crescent
+    local crescent_cube_right = translate(r_a + c, meshDistance + m * 2, 0) * cube(crescent_root_radius + c, crescent_root_radius + c, b + 0.1);  -- Right cube to remove sharp edges of the crescent
+    local crescent_cube_left = translate(-(r_a), meshDistance + m * 2, 0) * cube(crescent_root_radius + c, crescent_root_radius + c, b + 0.1);  -- Left cube to remove sharp edges of the crescent
+    local crescent_main = translate(0, 0, b / 2 + 0.1) * difference(ccylinder(crescent_outer_radius - c * 2, b), crescent_circle);  -- Crescent with sharp edges
     emit(intersection(difference(crescent_main, crescent_cube_right), difference(crescent_main, crescent_cube_left)), 0); -- Crescent formation without sharp edges
     set_brush_color(0, 0.2, 0.2, 0.2); -- Set color for crescent and gear housing
 end
